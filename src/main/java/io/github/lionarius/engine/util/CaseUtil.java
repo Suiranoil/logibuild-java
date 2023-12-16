85d5e57d6784539e94048703b71a042291a338f5
Suiranoil <lionlolqwe@gmail.com>
2023-12-16 18:59:40 +0300

    improve to sentence case

--- src/main/java/io/github/lionarius/engine/util/CaseUtil.java
+++ src/main/java/io/github/lionarius/engine/util/CaseUtil.java
@@ -1,4 +1,4 @@
-package io.github.lionarius.engine.editor;
+package io.github.lionarius.engine.util;
 
 import lombok.experimental.UtilityClass;
 
@@ -6,11 +6,11 @@ import java.util.regex.Pattern;
 
 @UtilityClass
 public class CaseUtil {
-    private static final Pattern UPPERCASE_LETTER = Pattern.compile("([A-Z]{2,})|([A-Z]|\\d+)");
+    private static final Pattern UPPERCASE_LETTER = Pattern.compile("([A-Z]{2,})(?=[A-Z0-9]|$)|([A-Z])(?=[a-z0-9]|$)|\\d+");
 
     public static String toSentenceCase(String camelCaseString) {
         return camelCaseString.substring(0, 1).toUpperCase()
                + UPPERCASE_LETTER.matcher(camelCaseString.substring(1))
-                       .replaceAll(m -> m.group(1) != null ? " " + m.group(1) : " " + m.group(2).toLowerCase());
+                       .replaceAll(m -> " " + m.group().toLowerCase());
     }
 }
